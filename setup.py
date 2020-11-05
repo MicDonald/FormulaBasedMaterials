ebb57162dc095f0d3d028741c6cc86ffcb6bd8e2
MicDonald <MicDonald@users.noreply.github.com>
2020-11-05 13:45:03 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-FormulaBasedMaterials", # Replace with your own username
-    version="0.0.1",
+    name="pkg-FormulaBasedMaterials", # Replace with your own username
+    version="0.1.0",
     author="Michael, Yu-Chuan, Hsu",
     author_email="mk60503mk60503@gmail.com",
     description="A code for generating Formula-Based Materials into Voxel, STL files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MicDonald/FormulaBasedMaterials",
+    url="https://github.com/MicDonald/FormulaBasedMaterials/archive/v0.1.0.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'numpy',
         'matplotlib',
         'trimesh',
@@ -28,7 +28,6 @@ setuptools.setup(
         'skimage',
         'random',
         'sympy',
-          
       ],
     python_requires='>=3.6',
 )
